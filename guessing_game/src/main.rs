cfed9216c5a3061d2d1452fc4b21ed264e4d987b
ta08 <marl4dev@gmail.com>
2024-10-16 21:12:56 +0900

    add loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,24 +4,28 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess: String = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..101);
+        let mut guess: String = String::new();
+        let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-        let guess:u32 = guess.trim().parse().expect("Please input a number.");
+        let guess: u32 = guess.trim().parse().expect("Please input a number.");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Small!"),
-        Ordering::Equal => println!("Equal!"),
-        Ordering::Greater => println!("Greater")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small!"),
+            Ordering::Greater => println!("Greater"),
+            Ordering::Equal => {
+                println!("Equal!");
+                break;
+            }
+        }
+        println!("Actual: {}", secret_number);
     }
-
-    println!("Actual: {}", secret_number);
 }

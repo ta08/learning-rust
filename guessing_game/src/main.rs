0583b60be65cfdc3776d8d42636bb2ff5640128c
ta08 <marl4dev@gmail.com>
2024-10-16 21:07:27 +0900

    add comparing number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,27 @@
-use std::io;
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess.trim());
+        let guess:u32 = guess.trim().parse().expect("Please input a number.");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Small!"),
+        Ordering::Equal => println!("Equal!"),
+        Ordering::Greater => println!("Greater")
+    }
+
+    println!("Actual: {}", secret_number);
 }

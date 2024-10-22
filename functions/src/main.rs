49d7f18f3e0f3311545209ed4046ee0b0ceed67f
ta08 <marl4dev@gmail.com>
2024-10-22 19:35:13 +0900

    finish chapter 3-3

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    another_function(20);
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+
+    println!("five() returns: {}", five());
+}
+
+fn another_function(x: i32) {
+    println!("Another Hello, world!, {} century", x);
+}
+fn five() -> i32 {
+    5
 }
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,12 @@
 
 fn main() {
     println!("Hello, world!");
+
+    let xs = [1,2,3,];
+    let [x ,y , z] = xs;
+    println!("x: {}, y: {}, z: {}", x, y, z);
+
+    let xs = (1,2,3,);
+    let (x ,y , z) = xs;
+    println!("x: {}, y: {}, z: {}", x, y, z);
 }

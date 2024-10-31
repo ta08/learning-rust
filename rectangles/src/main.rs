03bba829ff42ee833f3dc72e75eaeefb012cad6f
ta08 <marl4dev@gmail.com>
2024-10-31 22:51:44 +0900

    add rising a problem code

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 30;
+    let height = 20;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width, height)
+    );
+}
+
+fn area(width: usize, height: usize) -> usize {
+    width * height
 }

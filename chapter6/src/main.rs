811da80d38448674cd7d3861121176406cb5fded
ta08 <marl4dev@gmail.com>
2024-11-11 23:06:49 +0900

    finish chapter6-1

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -2,10 +2,30 @@ fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
     println!("Hello, world! {:?}", four);
+
+    let guess = Some(5);
+
+    let maybeSquare = match guess {
+        Some(x) => x * x,
+        None => 0,
+    };
+    println!("The square of the guess is: {}", maybeSquare);
 }
 
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
     V6,
+}
+
+enum IpAddr {
+    V4(u8,u8,u8,u8),
+    V6(u8,u8,u8,u8),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }

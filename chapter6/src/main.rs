4030b6db61113ad0785d803bdba7a11545f0da52
ta08 <marl4dev@gmail.com>
2024-11-11 23:23:41 +0900

    finish chapter6

--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -10,6 +10,31 @@ fn main() {
         None => 0,
     };
     println!("The square of the guess is: {}", maybeSquare);
+
+    let some_u8_value = Some(0u8);
+
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => println!("Nothing to do")
+    }
+
+    if let Some(x) = some_u8_value {
+        println!("Some number {}", x);
+    } else {
+        println!("Nothing to do 2");
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("Some number ");
+    } else {
+        println!("Nothing to do 2");
+    }
+
 }
 
 #[derive(Debug)]
@@ -19,8 +44,8 @@ enum IpAddrKind {
 }
 
 enum IpAddr {
-    V4(u8,u8,u8,u8),
-    V6(u8,u8,u8,u8),
+    V4(u8, u8, u8, u8),
+    V6(u8, u8, u8, u8),
 }
 
 enum Message {

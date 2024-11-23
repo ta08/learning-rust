c0ffb7dad83f53c46d1b59d2d49ff5c9118bcc2a
ta08 <marl4dev@gmail.com>
2024-11-23 22:36:56 +0900

    finish chapter83

--- chapter8/src/chapter83.rs
+++ chapter8/src/chapter83.rs
@@ -13,5 +13,12 @@ pub mod chapter83mod {
         scores.insert(String::from("Yellow"), 30);
 
         println!("{:?}", scores);
+
+        for (key, value) in &scores {
+            println!("{}: {}", key, value);
+        }
+
+        scores.entry(String::from("Yellow")).or_insert(20);
+        println!("{:?}", scores);
     }
 }

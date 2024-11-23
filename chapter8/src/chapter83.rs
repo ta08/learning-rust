7e57d8a8e2dee79d0fba13ae8bb7d01eda8110c0
ta08 <marl4dev@gmail.com>
2024-11-23 18:54:18 +0900

    overwrite hashmap with same key

--- chapter8/src/chapter83.rs
+++ chapter8/src/chapter83.rs
@@ -3,7 +3,15 @@
  */
 
 pub mod chapter83mod {
-    pub fn chapter83function(){
-        print!("from chapter83");
+    pub fn chapter83function() {
+        use std::collections::HashMap;
+
+        let mut scores = HashMap::new();
+
+        scores.insert(String::from("Blue"), 10);
+        scores.insert(String::from("Yellow"), 50);
+        scores.insert(String::from("Yellow"), 30);
+
+        println!("{:?}", scores);
     }
 }

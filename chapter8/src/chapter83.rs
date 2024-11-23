23dcc1b69dc7fb4674ee7a93e7c52832a92200e8
ta08 <marl4dev@gmail.com>
2024-11-24 00:52:34 +0900

    add mean and median functions

--- chapter8/src/chapter83.rs
+++ chapter8/src/chapter83.rs
@@ -2,7 +2,10 @@
  * Create
  */
 
+
 pub mod chapter83mod {
+    use itertools::Itertools;
+
     pub fn chapter83function() {
         use std::collections::HashMap;
 
@@ -20,5 +23,28 @@ pub mod chapter83mod {
 
         scores.entry(String::from("Yellow")).or_insert(20);
         println!("{:?}", scores);
+
+        let xs = vec![1,3,5,2,9,10];
+    
+        println!("mean result {:?}", mean(&xs));
+
+        // let xs = vec![1,3,5,2,9,10];
+        println!("median result {:?}", median(xs));
+    }
+
+
+    fn mean(list: &Vec<i32>) -> f64 {
+        let total: i32 = list.iter().sum();
+        total as f64  / list.len() as f64
+    }
+
+    fn median(mut list: Vec<i32>) -> f64 {
+        list.sort();
+        let mid = list.len() / 2;
+        if list.len() % 2 == 0 {
+            (list[mid - 1] as f64 + list[mid] as f64) / 2.0
+        } else {
+            list[mid] as f64
+        }
     }
 }

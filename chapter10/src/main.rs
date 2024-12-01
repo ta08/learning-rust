9fb50ed9866b8caaad4803dcb33c8e76f0d69f1b
ta08 <marl4dev@gmail.com>
2024-12-01 21:20:16 +0900

    modify compiler error

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -13,13 +13,11 @@ fn largest(list: &[i32]) -> i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut result = largest(&number_list);
-
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let mut result = largest(&number_list);
-
-    println!("The largest number is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }

ebfc252532f3cd6a6e273119b3f8522b12c06288
ta08 <marl4dev@gmail.com>
2024-12-19 23:21:35 +0900

    unpack a point

--- chapter10-1/src/main.rs
+++ chapter10-1/src/main.rs
@@ -1,3 +1,21 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup(self, other: Point<T, U>) -> Point<T, U> {
+        Point { x: self.x, y: other.y }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = Point { x: 5, y: 10 };
+    let y = Point { x: 11, y: 12 };
+    let Point { x: a, y: b } = x;
+
+    println!("{:?}", x.x);
+    println!("{:?}", x.x);
+    println!("{:?}", x.y);
+
 }
